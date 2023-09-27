f0766707151e9c45374bbc1ec6208755b3739aa0
meiji163 <meiji163@github.com>
2023-09-27 09:17:59 -0700

    wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,26 @@
 (ns wonderland-number.finder)
 
+(defn digits [^long n]
+  (re-seq #"\d" (str n)))
+
+(defn permutation? [l1 l2]
+  (= (sort l1) (sort l2)))
+
+(defn wonderland? [^long n]
+  (let [nums [(* 2 n) (* 3 n) (* 4 n) (* 5 n) (* 6 n)]
+        n-digs (digits n)]
+    (every? true?
+     (map #(permutation? (digits %) n-digs)
+      nums))
+    ))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (loop [n 100000]
+    (when (< n 166667)
+      (if (wonderland? n)
+        n
+        (recur (inc n))))
+    ))
+
+(wonderland-number)
+;; => 142857
